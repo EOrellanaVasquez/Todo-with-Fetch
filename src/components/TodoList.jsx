23ce9a376a25a69f151eb323fba4361b0487b298
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-08-28 23:44:58 -0400

    spleepy

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,78 +1,93 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import TodoForm from './TodoForm';
 import './TodoList.css';
 import Todo from './Todo';
 
-
 const TodoList = () => {
+  const apiUrl = 'https://playground.4geeks.com/apis/fake/todos/user/Arisen';
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-    const [todos, setTodos] = useState([]);
-    const [newTodo, setNewTodo] = useState('');
+  useEffect(() => {
+    getTodos();
+  }, [newTodo]);
 
-    useEffect(() => {
-        getTodos();
-    }, [newTodo])
+  const putTodos = (apiUrl, options = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' }
+  }) => {
+    return fetch(apiUrl, options)
+      .then((response) => {
+        return response.json();
+      });
+  };
 
-
-    const sendTodos = async (allTodos) => {
-        const response = await fetch(
-          'https://playground.4geeks.com/apis/fake/todos/user/Arisen',
-          {
-            method: "PUT",
-            body: JSON.stringify(allTodos),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-
-    const addTodo = todo => {
-        if (todo.text.trim()) {
-            todo.text = todo.text.trim();
-            const actualTodo = [todo, ...todos];
-            setTodos(actualTodo);
-        }
-
-    };
-
-    const deleteTodo = id => {
-        const actualTodo = todos.filter(todos => todos.id !== id);
-        setTodos(actualTodo);
+  const addTodo = (todo) => {
+    if (todo.text.trim()) {
+      todo.text = todo.text.trim();
+      const actualTodo = [todo, ...todos];
+      setTodos(actualTodo);
+      setNewTodo('');
+      putTodos(apiUrl, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(actualTodo)
+      });
     }
+  };
 
+  const getTodos = () => {
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.log(error.message));
+  };
 
-    const fullfilTodo = id => {
-        const actualTodo = todos.map(todos => {
-            if (todos.id === id) {
-                todos.fullfiled = !todos.fullfiled;
-            }
+  const deleteTodo = (id) => {
+    const actualTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(actualTodo);
+    putTodos(apiUrl, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(actualTodo)
+    });
+  };
 
-            return todos;
-        });
-        setTodos(actualTodo);
-    }
+  const fulfillTodo = (id) => {
+    const actualTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.fulfilled = !todo.fulfilled;
+      }
+      return todo;
+    });
+    setTodos(actualTodo);
+    putTodos(apiUrl, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(actualTodo)
+    });
+  };
 
+  return (
+    <>
+      <TodoForm onSubmit={addTodo} setNewTodo={setNewTodo} newTodo={newTodo} />
+      <div className="todo-list-container">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            fulfilled={todo.fulfilled}
+            fulfillTodo={fulfillTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-
-    return (
-        <>
-            <TodoForm onSubmit={addTodo} />
-            <div className='todo-list-container'>
-                {
-                    todos.map((todos) =>
-                        <Todo
-                            key={todos.id}
-                            id={todos.id}
-                            text={todos.text}
-                            fullfiled={todos.fullfiled}
-                            fullfilTodo={fullfilTodo}
-                            deleteTodo={deleteTodo} />
-                    )
-                }
-            </div>
-        </>
-    )
-}
-
-export default TodoList
+export default TodoList;
