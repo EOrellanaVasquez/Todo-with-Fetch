933b8fbf669e618a9c0fad3435cb5d92e8063b97
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-08-28 22:24:43 -0400

    commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoForm from './TodoForm';
 import './TodoList.css';
 import Todo from './Todo';
 
+
 const TodoList = () => {
 
     const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+
+    useEffect(() => {
+        getTodos();
+    }, [newTodo])
+
+
+    const sendTodos = async (allTodos) => {
+        const response = await fetch(
+          'https://playground.4geeks.com/apis/fake/todos/user/Arisen',
+          {
+            method: "PUT",
+            body: JSON.stringify(allTodos),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
 
     const addTodo = todo => {
-        if (todo.text.trim()){
+        if (todo.text.trim()) {
             todo.text = todo.text.trim();
             const actualTodo = [todo, ...todos];
             setTodos(actualTodo);
         }
-    
+
     };
 
     const deleteTodo = id => {
@@ -24,10 +44,10 @@ const TodoList = () => {
 
     const fullfilTodo = id => {
         const actualTodo = todos.map(todos => {
-            if (todos.id === id){
+            if (todos.id === id) {
                 todos.fullfiled = !todos.fullfiled;
             }
-            
+
             return todos;
         });
         setTodos(actualTodo);
@@ -35,24 +55,24 @@ const TodoList = () => {
 
 
 
-  return (
-    <>
-      <TodoForm onSubmit={addTodo} />
-        <div className='todo-list-container'>
-            {
-                todos.map((todos) => 
-                <Todo 
-                key={todos.id}
-                id={todos.id}
-                text={todos.text} 
-                fullfiled={todos.fullfiled}
-                fullfilTodo={fullfilTodo} 
-                deleteTodo={deleteTodo} />
-                )
-            }
-        </div>
-    </>
-  )
+    return (
+        <>
+            <TodoForm onSubmit={addTodo} />
+            <div className='todo-list-container'>
+                {
+                    todos.map((todos) =>
+                        <Todo
+                            key={todos.id}
+                            id={todos.id}
+                            text={todos.text}
+                            fullfiled={todos.fullfiled}
+                            fullfilTodo={fullfilTodo}
+                            deleteTodo={deleteTodo} />
+                    )
+                }
+            </div>
+        </>
+    )
 }
 
 export default TodoList
